5d8818e6ec41b77ce3efc8fc13a53c2577f931be
Riemann Bernard <riemann_b@gfa-lc6-64.psi.ch>
2020-02-13 17:30:30 +0100

    cleanup

--- main.py
+++ main.py
@@ -45,14 +45,6 @@ def plotJx(ax, tm : TuneMap, jX, grayMax=3.0):
                 faceLims=((-10, 0), (3, 10000)), faceColors=('#cccccc', '#cccccc'))
 
 
-def plot_multp(ax, u, b, k, m=None, sVar='s', dipoleColor='black', quadColor='xkcd:ocean blue', sextColor='xkcd:mustard'):
-    ax.plot(u, b, label='$b(%c)$' % sVar, linewidth=0.7, color=dipoleColor)
-    ax.plot(u, k, label='$k(%c)$' % sVar, linewidth=0.7, color=quadColor)
-    if m is not None:
-        ax.plot(u, m, label='$m(%c)$' % sVar, linewidth=0.7, color=sextColor)
-    [ax.spines[dr].set_color(None) for dr in ('top', 'right')]
-
-
 def plotMultipoles(ax, gr : Grapher, sVar='s', sextupoles=False):
     plot_multp(ax, 2*gr.sL, gr.b, gr.k, gr.mSext if sextupoles else None, sVar=sVar)
     ax.axhline(0, color='black', linestyle='dashed', linewidth=0.5)
@@ -116,37 +108,6 @@ def b1scan(axA, axB, nuX=0.45, nuY=0.35, minim=False, b1range=-arange(0,2.5,0.02
     for a in (axA, axB):    
         [a.spines[dr].set_color(None) for dr in ('top', 'right')]
 
-
-def multicolor_ylabel(ax, list_of_strings, list_of_colors, anchorpad=0, **kw):
-    """from https://stackoverflow.com/questions/33159134/matplotlib-y-axis-label-with-multiple-colors
-    this function creates axes labels with multiple colors
-    ax specifies the axes object where the labels should be drawn
-    list_of_strings is a list of all of the text items
-    list_if_colors is a corresponding list of colors for the strings
-    axis='x', 'y', or 'both' and specifies which label(s) should be drawn"""
-    from matplotlib.offsetbox import AnchoredOffsetbox, TextArea, HPacker, VPacker
-
-    boxes = [TextArea(text, textprops=dict(color=color, ha='left', va='bottom', rotation=90, **kw))
-             for text, color in zip(list_of_strings[::-1], list_of_colors[::-1])]
-    ybox = VPacker(children=boxes, align="center", pad=0, sep=10)
-    anchored_ybox = AnchoredOffsetbox(loc=3, child=ybox, pad=anchorpad, frameon=False, bbox_to_anchor=(-0.2, 0.3),
-                                      bbox_transform=ax.transAxes, borderpad=0.)
-    ax.add_artist(anchored_ybox)
-
-
-def opticsPlot(axBeta, s, betaX, betaY, etaX, betaXcolor='xkcd:royal blue', betaYcolor='0.4', etaColor='red', prefix='', etaLim=2.5):
-    axEta = axBeta.twinx() 
-    axBeta.plot(s, betaX, color=betaXcolor, label='x', linewidth=1)
-    axBeta.plot(s, betaY, color=betaYcolor, label='y', linewidth=1)
-    multicolor_ylabel(axBeta, (r'$%s\beta_x$' % prefix, r'$%s\beta_y$' % prefix), (betaXcolor, betaYcolor))
-    axEta.plot(s, etaX, color=etaColor, linewidth=1)
-    axEta.set(ylim=(0, etaLim), yticks=arange(0, etaLim + 1))
-    axEta.set_ylabel(r'$%s\eta_x$' % prefix, color=etaColor)
-
-    axBeta.spines['top'].set_color(None)
-    axEta.spines['top'].set_color(None)
-    axBeta.spines['right'].set_color(None)
-    axEta.spines['left'].set_color(None)
 
 
 def opaExport(filename, gr : Grapher, cellLength, curvature, energyGeV=2.4):
